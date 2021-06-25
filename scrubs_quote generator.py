6b8199f0fb3f16349913503889fb92b016449bcb
bmainock <57124447+bmainock@users.noreply.github.com>
2021-06-25 07:33:19 -0500

    omitting the list
    enabled voice and omitted the outputting of every quote

--- scrubs_quote generator.py
+++ scrubs_quote generator.py
@@ -4,42 +4,36 @@ import requests
 import random
 from bs4 import BeautifulSoup
 
-
 def theQuotes():
-    response = requests.get('https://www.quotes.net/movies/scrubs_106070')
 
-    #response = requests.get(webaddress)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get('https://www.quotes.net/movies/scrubs_106070') #Website with best list of quotes
+    copied_site = BeautifulSoup(response.text, 'html.parser')
 
-    # direct
-    snippet = soup.find_all(class_='disp-mquote-int')
+    html_quote_snippets = copied_site.find_all(class_='disp-mquote-int')
+    quote_list = []
 
-    # p = soup.find_all('p')
-
-    paragraphs = []
-    for x in snippet:
-        paragraphs.append(str(x))
-
-    paragraphs.pop()
-    print(len(paragraphs))
-
-    for y in range(len(paragraphs)):
-        paragraphs[y] = paragraphs[y].replace('<p>', '')
-        paragraphs[y] = paragraphs[y].replace('</p><strong>', '')
-        paragraphs[y] = paragraphs[y].replace('</p>', '')
-        paragraphs[y] = paragraphs[y].replace('</strong><br/>', '')
-        paragraphs[y] = paragraphs[y].replace('<strong>', '')
-        paragraphs[y] = paragraphs[y].replace('<a href="/mquote/"><strong>', '')
-        paragraphs[y] = paragraphs[y].replace('<div class="disp-mquote-int">', '')
-        paragraphs[y] = paragraphs[y].replace('</a>\n</div>', '')
-        paragraphs[y] = paragraphs[y][26:]
-        print(paragraphs[y], "\n")
+    for x in html_quote_snippets:
+        quote_list.append(str(x))
+    quote_list.pop()
+    #print(len(quote_list))
+    for y in range(len(quote_list)):
+        quote_list[y] = quote_list[y].replace('<p>', '')
+        quote_list[y] = quote_list[y].replace('</p><strong>', '')
+        quote_list[y] = quote_list[y].replace('</p>', '')
+        quote_list[y] = quote_list[y].replace('</strong><br/>', '')
+        quote_list[y] = quote_list[y].replace('<strong>', '')
+        quote_list[y] = quote_list[y].replace('<a href="/mquote/"><strong>', '')
+        quote_list[y] = quote_list[y].replace('<div class="disp-mquote-int">', '')
+        quote_list[y] = quote_list[y].replace('</a>\n</div>', '')
+        quote_list[y] = quote_list[y][26:]
+        #print(quote_list[y], "\n")
         #to print every quote on the list uncomment the code above
 
     # Navigation
-
+    chosen_one = random.choice(quote_list)
+    print(chosen_one)
     scrubs_quote = pyttsx3.init()
-    scrubs_quote.say(random.choice(paragraphs))
+    scrubs_quote.say(chosen_one)
     scrubs_quote.runAndWait()
 
 
